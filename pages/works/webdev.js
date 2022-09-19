096120b37e0e4e886d75f6b8c4f624aa2fd494f7
lawlesx <welcometohellv@gmail.com>
2022-09-19 19:48:00 +0530

    Added NFT Auction

--- pages/works/webdev.js
+++ pages/works/webdev.js
@@ -62,11 +62,14 @@ const appear = {
 
 const Webdev = () => {
 
+  // TODO: Requires a lot of refactoring
   const [modal, setModal] = useState(false);
   const [modal1, setModal1] = useState(false);
+  const [modal2, setModal2] = useState(false)
 
   const [hover, setHover] = useState(false);
   const [hover1, setHover1] = useState(false);
+  const [hover2,setHover2] = useState(false)
 
 
   return ( 
@@ -75,7 +78,6 @@ const Webdev = () => {
           <title>Web Development</title>
         </Head>
       <motion.div variants={appear} initial='hidden' animate='visible' exit='hidden' layoutId='webdev' className={styles.parentContainer}>
-        
         <AnimatePresence>
         {
           modal && <motion.div variants={backdrop} initial='hidden' animate='visible' exit='hidden' className={styles.backdrop}>
@@ -101,26 +103,39 @@ const Webdev = () => {
               </motion.div>
             </motion.div>
         }
+        {
+          modal2 && <motion.div variants={backdrop} initial='hidden' animate='visible' exit='hidden' className={styles.backdrop}>
+            <h1>A concept website for a NFT auction</h1>
+            <Link href='https://github.com/lawlesx/the-witch-trials-frontend'><a target="_blank" rel="noopener noreferrer">Github Repo</a></Link>
+            <Link href='https://the-witch-trials.vercel.app/'><a target="_blank" rel="noopener noreferrer">Live Link</a></Link>
+              <motion.div variants={crossVariants} whileHover='hover' className={styles.crossSection}
+                onClick={() => setModal2(false)}
+              >
+                <div className={styles.cline1}></div>
+                <div className={styles.cline2}></div>
+              </motion.div>
+            </motion.div>
+        }
         </AnimatePresence>
         <div className={styles.frame}>
-          {!hover && <h1>Web Dev</h1>}
+          {![!hover, !hover1 ,!hover2].includes(false) && <h1>Web Dev</h1>}
           <motion.div variants={imageVariant} animate={hover ? 'visible': 'hidden'} className={styles.image1}>
             <Image src='/Homepage.png' layout='fill' alt='Hack Club NMIT'/>
           </motion.div>
           <motion.div variants={imageVariant} animate={hover1 ? 'visible': 'hidden'} className={styles.image2}>
             <Image src='/Portfolio.png' layout='fill' alt='Portfolio'/>
           </motion.div>        
+          <motion.div variants={imageVariant} animate={hover2 ? 'visible': 'hidden'} className={styles.image3}>
+            <Image src='/The Witch Trials.png' layout='fill' alt='The Witch Trials'/>
+          </motion.div>
         </div>
         <motion.ul>
           <li><motion.a onClick={() => setModal(true)} onHoverStart={() => setHover(true)} onHoverEnd={() => setHover(false)}>Hack Club Nmit</motion.a></li>
           <li><motion.a onClick={() => setModal1(true)} onHoverStart={() => setHover1(true)} onHoverEnd={() => setHover1(false)}>My Portfolio</motion.a></li>
-          
-
+          <li><motion.a onClick={() => setModal2(true)} onHoverStart={() => setHover2(true)} onHoverEnd={() => setHover2(false)}>NFT Auction</motion.a></li>
         </motion.ul>
       </motion.div>
       </AnimatePresence>
-    
-    
    );
 }
  
