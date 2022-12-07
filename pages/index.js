7288caf02b54a0a1d842652925ac22b670cfa1ee
Aniruddha Sil <welcometohellv@gmail.com>
2022-12-07 22:15:05 +0530

    Added fonts to document level

--- pages/index.js
+++ pages/index.js
@@ -27,11 +27,6 @@ export default function Home() {
       <Head>
         <title>LAWLESX</title>
         <link rel="icon" href="/LX.png" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,500&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap" rel="stylesheet"/>
       </Head>
       <Intro />
       <Scroll />
