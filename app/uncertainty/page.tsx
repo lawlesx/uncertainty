632d5b5e12bbe29470e8500bbbb51e8ac94e1246
Aniruddha Sil <welcometohellv@gmail.com>
2023-01-16 21:52:36 +0530

    Added socials

--- app/uncertainty/page.tsx
+++ app/uncertainty/page.tsx
@@ -1,16 +1,20 @@
 import GetInTouch from "../../components/GetInTouch";
 import Intro from "../../components/Intro";
+import Socials from "../../components/Socials";
 
 export default function Page() {
 
   return (
-    <div className="w-full bg-background min-h-screen pl-16 pr-10">
-      <div className="flex flex-col w-full pr-32">
-        {/* ------------------------------ Get in Touch ------------------------------ */}
-        <div className="w-full py-10 flex items-center justify-end">
-          <GetInTouch />
+    <div className="w-full bg-background min-h-screen pl-16 pr-6">
+      <div className="flex">
+        <div className="flex flex-col w-full pr-24">
+          {/* ------------------------------ Get in Touch ------------------------------ */}
+          <div className="w-full py-10 flex items-center justify-end">
+            <GetInTouch />
+          </div>
+          <Intro />
         </div>
-        <Intro />
+        <Socials />
       </div>
     </div>
   )
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,12 +6,11 @@ const Intro = () => {
       <div className="flex flex-col">
         <h1 className="text-primary text-[8rem] leading-[8rem]">Hello</h1>
         <h1 className="text-primary text-[3rem] pt-2 pl-2">I am Aniruddha Sil</h1>
-        <p className="text-primary text-[2.3rem] font-quick pl-2">And this my Portfolio Website</p>
+        <p className="text-primary text-[2.3rem] font-quick pl-2">And welcome to my Portfolio Website</p>
       </div>
       <div>
         <Image src="/Profile.png" alt="Picture of the author" width={414} height={500} />
       </div>
-
     </div>
   )
 }
