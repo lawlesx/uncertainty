ea568526c94190d2c2e866d1c83c05b3348e0c38
Aniruddha Sil <welcometohellv@gmail.com>
2023-01-16 13:07:59 +0530

    Enabled experimental nextjs app dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#1a1a1a',
+        primary: '#f6f6f6',
+      },
+    },
   },
   plugins: [],
 }
